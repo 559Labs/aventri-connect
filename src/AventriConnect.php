688864d98686c37d2ae516b9b24f13213ccad198
Andrew Marconi <andrew@559labs.com>
2021-01-06 13:59:45 -0500

    Added doctum

--- src/AventriConnect.php
+++ src/AventriConnect.php
@@ -9,5 +9,22 @@ class AventriConnect
 
     use HasGlobalConcern;
 
+    // Data Container
+    protected $data = [
+        "endpoint" => "",
+    ];
+
+    // Constants
+    public const ENDPOINT_APAC = "https://api-apac.eventscloud.com";
+    public const ENDPOINT_NA = "https://api-na.eventscloud.com";
+    public const ENDPOINT_EMEA = "https://api-emea.eventscloud.com";
+
+
+    public function __construct($region=self::ENDPOINT_NA)
+    {
+
+    }
+
+
 
 }
