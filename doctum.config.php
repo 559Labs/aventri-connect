f2efe42258a34e653e37e5540cce83e9a38f3382
Andrew Marconi <andrew@559labs.com>
2021-01-07 22:12:29 -0500

    Added tdate to doc build

--- doctum.config.php
+++ doctum.config.php
@@ -4,6 +4,7 @@ use Doctum\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 use Doctum\Doctum;
 use Doctum\RemoteRepository\GitHubRemoteRepository;
+use Carbon\Carbon;
 
 $dir = __DIR__;
 
@@ -27,7 +28,7 @@ return new Doctum (
     $iterator,
     [
         "theme"                => "default",
-        "title"                => "Aventri Connect API",
+        "title"                => "Aventri Connect API: " . Carbon::now()->toDateString(),
         "build_dir"            => $dir . "/dist/build/%version%",
         "cache_dir"            => $dir . "/dist/cache/%version%",
         "default_opened_level" => 1,
