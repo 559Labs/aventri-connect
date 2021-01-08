3b79ce2f490107fb434e30362b1608bd87396ec6
Andrew Marconi <andrew@559labs.com>
2021-01-07 22:02:08 -0500

    Moved to public methods for development

--- src/Concerns/BaseService.php
+++ src/Concerns/BaseService.php
@@ -6,13 +6,12 @@ use FFNLabs\AventriConnect\Exceptions\BadRequestException;
 
 trait BaseService
 {
-    protected function request($method, $uri, $options = [])
+    public function request($method, $uri, $options = [])
     {
         $this->authorize();
         $request = $this->getClient()->request($method, $uri, $options);
         $body = json_decode($request->getBody());
         $this->data['last_request'] = $request;
-
         if ($request->getStatusCode() > 299) {
             throw new BadRequestException($request->getReasonPhrase());
         }
@@ -21,39 +20,33 @@ trait BaseService
             foreach ($body->error as $k => $v) {
                 $msg .= $k . ": " . $v . "; ";
             }
-
             throw new BadRequestException($msg);
         }
-
         return json_decode($request->getBody());
     }
 
-    protected function get($uri, $options = [])
+    public function get($uri, $options = [])
     {
         $this->authorize();
         $options = array_merge($options, ['query' => ['accesstoken' => $this->data['auth']['token']['value']]]);
-
         return $this->request("get", $uri, $options);
     }
 
-    protected function post($uri, $options = [])
+    public function post($uri, $options = [])
     {
         $this->authorize();
-
         return $this->request("post", $uri, $options);
     }
 
-    protected function patch($uri, $options = [])
+    public function patch($uri, $options = [])
     {
         $this->authorize();
-
         return $this->request("patch", $uri, $options);
     }
 
-    protected function put($uri, $options = [])
+    public function put($uri, $options = [])
     {
         $this->authorize();
-
         return $this->request("put", $uri, $options);
     }
 }
